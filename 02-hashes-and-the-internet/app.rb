512f0250f0e00768d415beb011ed1cf9fee6d344
kingcons <brit@kingcons.io>
2019-03-12 12:07:22 -0400

    Add finished but not refactored code.

--- 02-hashes-and-the-internet/app.rb
+++ 02-hashes-and-the-internet/app.rb
@@ -1,2 +1,24 @@
-# require 'rest-client'
-# require 'json'
+require 'rest-client'
+require 'json'
+require 'pry'
+
+puts "Welcome to Google Book Searcher"
+puts "What sort of books are you looking for today?"
+
+query = gets.chomp
+url = "https://www.googleapis.com/books/v1/volumes?q=#{query}"
+
+response = RestClient.get(url)
+data = JSON.parse(response.body)
+
+puts "\n\n**Search Results**\n\n"
+
+data['items'].each do |book_info|
+  title = book_info['volumeInfo']['title']
+  authors = book_info['volumeInfo']['authors'].join(" and ")
+  description = book_info['volumeInfo']['description'][0..140]
+  puts "=" * 30
+  puts "Title: #{title}"
+  puts "Authors: #{authors}"
+  puts "Description: #{description}"
+end
